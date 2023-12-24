465cf5add9e9fd0975fee1f36bc0b5a0d3a55aba
Zion <mrzz@tuta.io>
2023-12-24 16:29:16 +0100

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Set Safe
+This is a simple tool to set up Safe contracts on an EVM-compatible blockchain (not ZK one), to enable the MultiSig functionality on this blockchain.
+
+This tool depends on two repositories:
+* [safe-singleton-factory](https://github.com/safe-global/safe-singleton-factory)
+* [safe-contracts](https://github.com/safe-global/safe-contracts)
+
+
---
+++ deploy-safe.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+source .env
+git submodule update --init --recursive
+# Get ChainID
+hexChainID=$(curl --location --request POST "$NODE_URL" \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "jsonrpc": "2.0",
+    "method": "eth_chainId",
+    "params": [],
+    "id": 1
+}' | jq '.result')
+trimmedChainID=$(echo $hexChainID | tr -d '"')
+chainID=$(printf "%d\n" $trimmedChainID)
+echo "ChainID $chainID"
+
+pushd lib/safe-singleton-factory
+
+yarn install
+# Setup singleton env file
+printf "RPC=\"$RPC\"\n" > .env
+mkdir -p artifacts/$chainID
+printf "{
+	\"gasPrice\": 100,
+	\"gasLimit\": 100000,
+	\"signerAddress\": \"0x3fab184622dc19b6109349b94811493bf2a45362\",
+	\"transaction\": \"0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222\",
+	\"address\": \"0x4e59b44847b379578588920ca78fbf26c0b4956c\"
+}" > artifacts/$chainID/deployment.json
+
+# yarn submit
+
+popd
+
+pushd lib/safe-contracts
+git checkout v1.3.0-libs.0
+yarn install
+printf "PK=\"$PK\"\nNODE_URL=\"$RPC\"\n" > .env
+yarn deploy-all custom
+popd
+
+echo "Completed Safe contracts deployment"
