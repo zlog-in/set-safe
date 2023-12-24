72215332f0928d5fc48f49ed00fd6f911bbf0036
Zion <mrzz@tuta.io>
2023-12-24 17:12:41 +0100

    README add

--- README.md
+++ README.md
@@ -5,4 +5,22 @@ This tool depends on two repositories:
 * [safe-singleton-factory](https://github.com/safe-global/safe-singleton-factory)
 * [safe-contracts](https://github.com/safe-global/safe-contracts)
 
+## Set Up
+First, clone this  repository:
+```
+git clone https://github.com/zlog-in/set-safe.git && cd set-safe 
+```
+Then, set an .env file with the following variables:
+```
+export PK="your private key"
+export RPC="the rpc url of the blockchain"
+```
+Last, just run the bash script:
+```
+bash set-safe.sh
+```
+
+
+
+
 
--- deploy-safe.sh
+++ deploy-safe.sh
@@ -2,7 +2,7 @@
 source .env
 git submodule update --init --recursive
 # Get ChainID
-hexChainID=$(curl --location --request POST "$NODE_URL" \
+hexChainID=$(curl --location --request POST "$RPC" \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "jsonrpc": "2.0",
@@ -19,16 +19,20 @@ pushd lib/safe-singleton-factory
 yarn install
 # Setup singleton env file
 printf "RPC=\"$RPC\"\n" > .env
+
+
+gasPrice=$(yarn estimate | sed -n "s/.*gasPrice: '\([^']*\)'.*/\1/p")
+
 mkdir -p artifacts/$chainID
 printf "{
-	\"gasPrice\": 100,
+	\"gasPrice\": $gasPrice,
 	\"gasLimit\": 100000,
 	\"signerAddress\": \"0x3fab184622dc19b6109349b94811493bf2a45362\",
 	\"transaction\": \"0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222\",
 	\"address\": \"0x4e59b44847b379578588920ca78fbf26c0b4956c\"
 }" > artifacts/$chainID/deployment.json
 
-# yarn submit
+yarn submit
 
 popd
 
